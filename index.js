962a54140bf10e4ed9bf0ac9c30933d62d7329d4
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-12 01:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 var name = "Susan"
 
 var height = 39
-var message = `Pateint name is ${name} and patient is ${height} inches tall`
-var height = 74
-var message = `Name is ${name} and is ${height} inches tall`
+var message = `Patient name is ${name} and patient is ${height} inches tall`
+
 
 module.exports = {name, height, message}
